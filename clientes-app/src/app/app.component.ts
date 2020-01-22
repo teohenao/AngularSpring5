fbcfbbf0048147077dd2a75f8b25bae7974f7b3f
teohenao <m-teo770@hotmail.com>
2020-01-22 17:59:12 -0500

    creando el proyecto

---
+++ clientes-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a angular';
+  curso: string = 'Curso Spring 5 con angular 8';
+  estudiante: string = 'Mateo Henao Rodriguez'
+}

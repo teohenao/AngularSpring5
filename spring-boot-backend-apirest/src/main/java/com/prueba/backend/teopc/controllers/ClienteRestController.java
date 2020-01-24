840414c7d4bc9bba951c9ce36a05411b3d1d71d5
teohenao <m-teo770@hotmail.com>
2020-01-23 22:08:21 -0500

    cors conexion back y front

--- spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.prueba.backend.teopc.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import com.prueba.backend.teopc.models.services.IClienteService;
  * el map es la direccion de la app con la que quiere iniciar y ya despues hacer los request
  * @author mateohenao
  */
+//cross es el encargado de la comunicacion entre el back y el front se puede especificar en cross los request que le voy a permitir
+@CrossOrigin(origins = {"http://localhost:42000"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {

5d163096b6400cb3b7133d994da1ffa395551fee
teohenao <m-teo770@hotmail.com>
2020-01-23 22:39:21 -0500

    escribiendo los metodos de crud en el backend

--- spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/models/services/ClienteServiceImplement.java
+++ spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/models/services/ClienteServiceImplement.java
@@ -19,11 +19,31 @@ public class ClienteServiceImplement implements IClienteService {
 	@Autowired
 	private IClienteDao clienteDao;
 	
+	//en el transactional se coloca que desea, si solo lectura o escritura o todos etc
 	@Override
 	@Transactional(readOnly = true)
 	public List<Cliente> findAll() 
 	{
 		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		//orElse, si lo encuentra retorna un cliente de lo contrario null
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
 	}
 
 }
--- spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/models/services/IClienteService.java
+++ spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/models/services/IClienteService.java
@@ -6,6 +6,16 @@ import com.prueba.backend.teopc.models.entity.Cliente;
 
 public interface IClienteService {
 	
+	//obtener todos los clientes
 	public List<Cliente> findAll();
+	
+	//obtener un cliente por medio del id
+	public Cliente findById(Long id);
+	
+	//guardar un cliente
+	public Cliente save(Cliente cliente);
+	
+	//eliminar un cliente
+	public void delete(Long id);
 
 }

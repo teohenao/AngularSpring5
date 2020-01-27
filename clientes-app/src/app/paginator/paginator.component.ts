5c7a2bca2fc1d7d91e8a926cb0516b9b8302390c
teohenao <m-teo770@hotmail.com>
2020-01-27 09:35:06 -0500

    rango de paginador, ciclo de vida comp onchange

--- clientes-app/src/app/paginator/paginator.component.ts
+++ clientes-app/src/app/paginator/paginator.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input ,OnChanges} from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit,OnChanges {
   
   // con Input se coloca para injeccion de dependencias entre hijo y padre 
   @Input() paginador:any;
@@ -12,12 +12,31 @@ export class PaginatorComponent implements OnInit {
   //arreglo de numeros de la cantidad de paginas
   paginas:number[];
 
+  //rangos del paginador
+  desde:number;
+  hasta:number;
+
   constructor() { }
   ngOnInit() {
-    //total pages es un atributo que viene desde el paginador json , se le coloca un _ al valor que no usemos para que no chille
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor,indice)=>
-      indice+1
-    );
+  
+  }
+
+  ngOnChanges()
+  {
+      //rangos del paginador para cuando es mayor a 5
+      this.desde = Math.min(Math.max(1,this.paginador.number-4),this.paginador.totalPages-5);
+      this.hasta = Math.max(Math.min(this.paginador.totalPages,this.paginador.number+4),6);
+      if(this.paginador.totalPages>5)
+      {
+        this.paginas = new Array(this.hasta -this.desde+1).fill(0).map((_valor,indice)=>
+        indice+this.desde
+      );
+      }else{
+      //total pages es un atributo que viene desde el paginador json , se le coloca un _ al valor que no usemos para que no chille
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor,indice)=>
+        indice+1
+      );
+      }
   }
 
 }

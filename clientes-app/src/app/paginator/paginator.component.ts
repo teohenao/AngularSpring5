f6606045b6946196fa7a6a615758b69e67087845
teohenao <m-teo770@hotmail.com>
2020-01-27 09:42:20 -0500

    finalizando rangos de paginador front

--- clientes-app/src/app/paginator/paginator.component.ts
+++ clientes-app/src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input ,OnChanges} from '@angular/core';
+import { Component, OnInit, Input ,OnChanges,SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
@@ -18,25 +18,35 @@ export class PaginatorComponent implements OnInit,OnChanges {
 
   constructor() { }
   ngOnInit() {
-  
+    this.initPaginator();
   }
 
-  ngOnChanges()
+  //a travez del change podemos obtener el campo del input paginador que tenemos declarado
+  ngOnChanges(changes:SimpleChanges)
   {
-      //rangos del paginador para cuando es mayor a 5
-      this.desde = Math.min(Math.max(1,this.paginador.number-4),this.paginador.totalPages-5);
-      this.hasta = Math.max(Math.min(this.paginador.totalPages,this.paginador.number+4),6);
-      if(this.paginador.totalPages>5)
-      {
-        this.paginas = new Array(this.hasta -this.desde+1).fill(0).map((_valor,indice)=>
-        indice+this.desde
-      );
-      }else{
-      //total pages es un atributo que viene desde el paginador json , se le coloca un _ al valor que no usemos para que no chille
-      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor,indice)=>
-        indice+1
-      );
-      }
+     let paginadorActualizaco = changes['paginador'];
+     //si paginador actualizado tiene un estado anterior y haya cambiado
+     if(paginadorActualizaco.previousValue)
+     {
+      this.initPaginator();
+     }
+  }
+
+  private initPaginator():void{
+     //rangos del paginador para cuando es mayor a 5
+     this.desde = Math.min(Math.max(1,this.paginador.number-4),this.paginador.totalPages-5);
+     this.hasta = Math.max(Math.min(this.paginador.totalPages,this.paginador.number+4),6);
+     if(this.paginador.totalPages>5)
+     {
+       this.paginas = new Array(this.hasta -this.desde+1).fill(0).map((_valor,indice)=>
+       indice+this.desde
+     );
+     }else{
+     //total pages es un atributo que viene desde el paginador json , se le coloca un _ al valor que no usemos para que no chille
+     this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor,indice)=>
+       indice+1
+     );
+     }
   }
 
 }

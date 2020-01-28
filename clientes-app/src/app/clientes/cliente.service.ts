152273ca0645cc7e31dc895d1d04701a5b644ea9
teohenao <m-teo770@hotmail.com>
2020-01-28 16:46:56 -0500

    mostrar foto y detalles de cliente front detalle

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -129,7 +129,7 @@ subirFoto(archivo:File, id):Observable<Cliente>
   let formData = new FormData();
   formData.append("archivo",archivo);
   formData.append("id",id);
-  return this.http.post(`${this.urlEndPoint}/upload/`,formData).pipe(
+  return this.http.post(`${this.urlEndPoint}/upload`,formData).pipe(
     map((response:any)=>response.cliente as Cliente),
     catchError(e=>{
       console.error(e.error.mensaje);

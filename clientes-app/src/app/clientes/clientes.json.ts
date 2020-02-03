50841a1faa554f30229b5d21bf0eca20c0b790c1
teohenao <m-teo770@hotmail.com>
2020-02-02 22:08:40 -0500

    estilos y animacion de modal front

--- clientes-app/src/app/clientes/clientes.json.ts
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -1,7 +1,7 @@
 import { Cliente } from "./cliente";
 
-export const CLIENTES: Cliente[]=[
-    {id:1,nombre:'Andres',apellido:'Rodriguez',createAt:'2019-12-11',email:'andres@gmail.com'},
-    {id:2,nombre:'Maria',apellido:'Perez',createAt:'2011-12-12',email:'Maria@gmail.com'},
-    {id:3,nombre:'Andres3',apellido:'Rodriguez3',createAt:'2013-12-11',email:'andres3@gmail.com'}
-  ];
+// export const CLIENTES: Cliente[]=[
+//     {id:1,nombre:'Andres',apellido:'Rodriguez',createAt:'2019-12-11',email:'andres@gmail.com'},
+//     {id:2,nombre:'Maria',apellido:'Perez',createAt:'2011-12-12',email:'Maria@gmail.com'},
+//     {id:3,nombre:'Andres3',apellido:'Rodriguez3',createAt:'2013-12-11',email:'andres3@gmail.com'}
+//   ];

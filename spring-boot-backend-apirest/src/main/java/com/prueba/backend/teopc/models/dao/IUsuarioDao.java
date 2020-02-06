31811eaf7d0cdebd98d6ee7cf075dd1a28db1a30
teohenao <m-teo770@hotmail.com>
2020-02-05 22:07:51 -0500

    creando repositorio JPA IUsuarioDao

---
+++ spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/models/dao/IUsuarioDao.java
@@ -0,0 +1,20 @@
+package com.prueba.backend.teopc.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.prueba.backend.teopc.models.entity.Usuario;
+
+
+//estas interfaces son necesarias para utilizar el crud que tenemos en la libreria, existen varias como jpa repository y mas
+public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
+	
+	//la nombrada del metodo es importante si queremos pasar mas parametros podemos colocar findByUserNameAndEmailOrEjemplo, y asi y pasar los parametros que queramos asi	
+	public Usuario findByUsername(String username);
+	
+	//ejemplo con query 1 es el parametro uno o los que uno quiera pasar	
+	@Query("select u from Usuario u where u.username=?1 and u.otro?2")
+	public Usuario findByUsername2(String username);
+	
+
+}

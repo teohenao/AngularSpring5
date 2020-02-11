e6c317998bd3094638d62736cf29f1678fce1b0e
teohenao <m-teo770@hotmail.com>
2020-02-11 12:34:59 -0500

    configuracion clientes y accesos endponit

--- spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/auth/AutorizationServerConfig.java
+++ spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/auth/AutorizationServerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
@@ -44,19 +43,25 @@ public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapt
 		return jwtAccessTokenConverter;
 	}
 
-
-
-
-
+	//los permisos de los endPoin
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(security);
+		//permit all es para que todos puedan autorizarse, login puedan intentar entrar		
+		security.tokenKeyAccess("permitAll()")
+		//endpoint que verifica el token y su firma		
+		.checkTokenAccess("isAuthenticated()");
 	}
 
+	//se configuran los clientes, un cliente es el front que utiliza esta seguridad,	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(clients);
+		clients.inMemory().withClient("angularapp")
+		.secret(contraseniaEncriptada.encode("12345"))
+		.scopes("read","write")
+		//refresh token es para que no toque inciiar sesiion cada que se refresque
+		.authorizedGrantTypes("password","refresh_token")
+		//tiempo de caducidad del token
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
 	}
 }

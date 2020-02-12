421d7113dc8f56b21daaa85771261be483a6c256
teohenao <m-teo770@hotmail.com>
2020-02-12 11:03:18 -0500

    seguridad en rutas con HTTPSecurity

--- spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/auth/ResourceServerConfig.java
+++ spring-boot-backend-apirest/src/main/java/com/prueba/backend/teopc/auth/ResourceServerConfig.java
@@ -10,11 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	//METODO ENCARGADO DE LA SEGURIDA DE ROLES 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		//el orden es de lo mas especifico a lo mas generico de arriba hacia abajo en permisos		
 		//rutas publicas que cualquier usuario puede acceder este o no loggeado
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes").permitAll()
-		//estas se colocan siempre al final para todas las reglas
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes","/api/clientes/page/**","/api/uploads/img/**").permitAll()
+		//permite obtener a usuarios y admin
+		.antMatchers(HttpMethod.GET,"/api/clientes/{id}").hasAnyRole("ROL_USUARIO","ROL_ADMIN")
+		.antMatchers(HttpMethod.POST,"/api/clientes/upload").hasAnyRole("ROL_USUARIO","ROL_ADMIN")
+		//permite post a solo admin
+		.antMatchers(HttpMethod.POST,"/api/clientes").hasRole("ROL_ADMIN") 
+		//RUTA GENERICA, despues de lo que sigue de una ruta, va a ser de un rol, como un limite
+		.antMatchers("/api/clientes/**").hasRole("ROL_ADMIN")
+		//estas se colocan siempre al final para todas las reglas, significa que las rutas que no se colocaron aca necesitan tener autentificacion y no depende de rol
 		.anyRequest().authenticated();
 	}
 

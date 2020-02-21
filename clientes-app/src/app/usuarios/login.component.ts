af7bad2f6762a9eb7360681476a2edc8ba4f754c
teohenao <m-teo770@hotmail.com>
2020-02-21 13:29:02 -0500

    metodo login y creando usuario front

--- clientes-app/src/app/usuarios/login.component.ts
+++ clientes-app/src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo:string = 'Por favor inicie sesion';
+  usuario:Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+   }
 
   ngOnInit(): void {
   }
 
+  login():void
+  {
+    console.log(this.usuario);
+    if(this.usuario.username == null || this.usuario.password == null)
+    {
+      swal.fire('error login','user O password vacias','error')
+      return;
+    }
+  }
 }

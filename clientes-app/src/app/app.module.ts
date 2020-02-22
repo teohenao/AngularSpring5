1e1500cde577b6363285eaf1fb35f9aa5307c8e9
teohenao <m-teo770@hotmail.com>
2020-02-22 18:11:50 -0500

    guard router con roles

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
 import { AuthGuard } from './usuarios/guards/auth.guard';
+import { RoleGuard } from './usuarios/guards/role.guard';
 
 //internacionalizacion para el idioma de las fechas
 registerLocaleData(localeES,'es');
@@ -32,8 +33,8 @@ const routes:Routes=[
   {path:'directivas',component:DirectivaComponent},
   {path:'clientes',component:ClientesComponent},
   {path:'clientes/page/:page',component:ClientesComponent},
-  {path:'clientes/form',component:FormComponent,canActivate:[AuthGuard]},//canActive es un arreglo ya que se pueden tener varios guard si uno quiere
-  {path:'clientes/form/:id',component:FormComponent,canActivate:[AuthGuard]},
+  {path:'clientes/form',component:FormComponent,canActivate:[AuthGuard,RoleGuard],data:{role:'ROLE_ADMIN'}},//canActive es un arreglo ya que se pueden tener varios guard si uno quiere
+  {path:'clientes/form/:id',component:FormComponent,canActivate:[AuthGuard,RoleGuard],data:{role:'ROLE_ADMIN'}},
   {path:'login',component:LoginComponent}
 ];
 
--- clientes-app/src/app/usuarios/guards/role.guard.ts
+++ clientes-app/src/app/usuarios/guards/role.guard.ts
@@ -2,26 +2,31 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class RoleGuard implements CanActivate {
 
   constructor(private authService:AuthService,private route:Router)
   {
 
   }
-
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      //cumplida esta condicion determina si deja pasar o no, utilizar el authguard es redundancia, con este seria suficiente
-      if(this.authService.estaAutenticado())
-      {
+      //es necesario preguntar si inicio sesison, y si tiene el rol
+      if(!this.authService.estaAutenticado()){
+        this.route.navigate(['/login']);
+        return false;
+      }
+      let role = next.data['role'] as string;
+      if(this.authService.hasRole(role)){
         return true;
       }
-    this.route.navigate(['/login'])
+    Swal.fire('Acceso denegado','usted que hace por aca!','error');
+    this.route.navigate(['/clientes']);
     return false;
   }
   

e577ab7e8febca1743ee438b6886b2d49dc0021d
teohenao <m-teo770@hotmail.com>
2020-02-23 19:09:39 -0500

    vista detalle factura

--- clientes-app/src/app/facturas/detalle-factura.component.ts
+++ clientes-app/src/app/facturas/detalle-factura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FacturasService } from './services/facturas.service';
+import { Factura } from './models/factura';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-factura',
@@ -6,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleFacturaComponent implements OnInit {
 
-  constructor() { }
+  factura:Factura;
+  titulo:string = 'Factura';
+
+
+  constructor(private facturaService:FacturasService,private activateRoute:ActivatedRoute) { }
 
   ngOnInit() {
+    this.activateRoute.paramMap.subscribe(params=>{
+      let id = +params.get('id');
+      this.facturaService.getFactura(id).subscribe(factura=>{
+        this.factura = factura;
+      })
+    })
   }
+
 
 }
